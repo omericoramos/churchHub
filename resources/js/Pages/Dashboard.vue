28480dd0c89269314d9bcbe1b750718ab5944196
omericoramos <omericoramos@gmail.com>
2023-08-03 16:33:09 -0300

    testando github actions

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,7 +12,7 @@ import Register from '@/Components/Register.vue'
                 <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-2">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="bg-white grid grid-cols-5 shadow-sm sm:rounded-lg">
-                            <h2 class="font-semibold p-6 text-xl text-gray-800 leading-tight">Dashboardw este</h2>
+                            <h2 class="font-semibold p-6 text-xl text-gray-800 leading-tight">Dashboardw</h2>
                         </div>
                     </div>
                 </div>
